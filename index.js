caca05d39377e5cd73b32a56f4833b52e439a19b
wangch15 <harry19910202@gmail.com>
2022-09-16 20:31:57 +0800

    初次上傳

---
+++ index.js
@@ -0,0 +1,12 @@
+
+//swiper js
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
